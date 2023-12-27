25b5129fa4ff9530af1c99790f2ed3755734c8dc
Cliry1 <128536194+Cliry1@users.noreply.github.com>
2023-12-27 22:41:03 +0200

    new

---
+++ src/js/2-form.js
@@ -0,0 +1,28 @@
+const form = document.querySelector(".feedback-form");
+const storage = "feedback-form-state";
+
+
+if(localStorage.getItem(storage)){
+  form.elements.email.value=JSON.parse(localStorage.getItem(storage)).email.trim();
+  form.elements.message.value = JSON.parse(localStorage.getItem(storage)).message.trim();
+}
+else{
+  form.elements.email.value="";
+  form.elements.message.value="";
+}
+
+
+form.addEventListener("input", () =>{
+  localStorage.setItem(storage, JSON.stringify({email:form.elements.email.value, message:form.elements.message.value}))
+})
+
+form.addEventListener("submit", event=> {
+  event.preventDefault();
+  if(form.elements.message.value.trim()===""||form.elements.email.value.trim()===""){
+    alert("All form fields must be filled in");
+    return;
+  }
+  console.log(localStorage.getItem(storage))
+  localStorage.removeItem(storage);
+  form.reset();
+})
