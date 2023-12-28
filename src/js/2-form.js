8c9ca4e52d2f0c3e7a3a2bc300100e6689a28223
Cliry1 <128536194+Cliry1@users.noreply.github.com>
2023-12-28 11:12:57 +0200

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,10 +1,11 @@
 const form = document.querySelector(".feedback-form");
 const storage = "feedback-form-state";
 
+const storageItem = JSON.parse(localStorage.getItem(storage));
 
 if(localStorage.getItem(storage)){
-  form.elements.email.value=JSON.parse(localStorage.getItem(storage)).email.trim();
-  form.elements.message.value = JSON.parse(localStorage.getItem(storage)).message.trim();
+  form.elements.email.value=storageItem.email;
+  form.elements.message.value = storageItem.message;
 }
 else{
   form.elements.email.value="";
@@ -13,7 +14,7 @@ else{
 
 
 form.addEventListener("input", () =>{
-  localStorage.setItem(storage, JSON.stringify({email:form.elements.email.value, message:form.elements.message.value}))
+  localStorage.setItem(storage, JSON.stringify({email:form.elements.email.value.trim(), message:form.elements.message.value.trim()}))
 })
 
 form.addEventListener("submit", event=> {
